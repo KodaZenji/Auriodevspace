cfb137d9f2f54514420a1ef01b64590b7882621e
KodaZenji <2400072.benjamin@nict.edu.ng>
2025-09-20 08:38:14 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,11 +15,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Aurio Devspace and Tools",
   description: "Developer tools and utilities by Aurio 💚",
-  icons: {
-    icon: '/image.jpg', 
-    shortcut: '/image.jpg',
-    apple: '/image.jpg',
-}
 };
 export default function RootLayout({
   children,
