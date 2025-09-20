ce4125a16c99e6dacfde7431efeb1ead26df683b
KodaZenji <2400072.benjamin@nict.edu.ng>
2025-09-20 08:51:16 +0100

    Update layout.tsx

--- app/sonic-rewards-calculator/layout.tsx
+++ app/sonic-rewards-calculator/layout.tsx
@@ -15,7 +15,11 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Sonic x Kaito Yapper Rewards Calculator",
   description: "Estimate your S token rewards based on your leaderboard ranking in Round 1 of the Sonic Yapper campaign",
-  
+  icons: {
+  icon: '/sonic-labs-logo.png', 
+    shortcut: '/sonic-labs-logo.png',
+    apple: '/sonic-labs-logo.png',
+ } 
 };
 
 export default function RootLayout({
