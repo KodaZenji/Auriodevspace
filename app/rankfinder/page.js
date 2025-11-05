30779b91b08ea42db6fb392bdd45379e4202a929
KodaZenji <2400072.benjamin@nict.edu.ng>
2025-11-05 22:51:59 +0100

    Update page.js

--- app/rankfinder/page.js
+++ app/rankfinder/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import React, { useState, useEffect, useRef, useCallback, useMemo } from "react";
 import { RefreshCw, Info, SquareArrowOutUpRight } from "lucide-react";
 
