421c4a72b1499de11dcc98eeef58ebd8e97a9281
KodaZenji <2400072.benjamin@nict.edu.ng>
2025-11-14 21:46:34 +0100

    retest again

--- app/kaito-inner-ct/page.js
+++ app/kaito-inner-ct/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import React, { useState, useEffect } from "react";
 import { UserPlus, ChevronLeft, ChevronRight, LogOut, BadgeCheck } from "lucide-react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
