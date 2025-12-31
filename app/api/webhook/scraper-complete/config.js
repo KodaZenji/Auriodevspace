b7024afd5a03e2ab061dc0ae5d51a48ad915daaf
KodaZenji <2400072.benjamin@nict.edu.ng>
2025-12-31 02:24:26 +0100

    Create config.js

---
+++ app/api/webhook/scraper-complete/config.js
@@ -0,0 +1,20 @@
+
+
+export const PERIOD_TO_DAYS = {
+  'epoch-2': 2,
+  '7d': 7,
+  '30d': 30
+};
+
+export const LEADERBOARD_NAMES = {
+  YAPPERS: 'yappers',
+  DUELDUCK: 'duelduck',
+  ADICHAIN: 'adichain',
+  HEYELSA: 'heyelsa',
+  BEYOND: 'beyond',
+  MINDOSHARE: 'mindoshare',
+  HELIOS: 'helios',
+  SPACE: 'space',
+  DEEPNODEAI: 'deepnodeai',
+  C8NTINUUM: 'c8ntinuum'
+};
