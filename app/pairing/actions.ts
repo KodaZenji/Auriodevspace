b39292243c42bca2eff84e3228aa9c3feffd5091
KodaZenji <2400072.benjamin@nict.edu.ng>
2026-06-04 17:31:26 +0100

    Update actions.ts

--- app/pairing/actions.ts
+++ app/pairing/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 
-import { createClient } from '@/lib/supabase-server';
+import { createClient } from '@/lib/supabase';
+
 import { revalidatePath } from 'next/cache';
 
 export async function getProfiles() {
